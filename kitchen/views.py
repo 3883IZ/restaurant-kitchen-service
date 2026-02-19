33503e5578d4cdab04fb15df9423efe32c88ff9f
Ihor Zasimenko <boxletter2011@gmail.com>
2026-02-19 18:27:45 +0200

    Add search functionality for dishes

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-from .models import Category
+from .models import Category, Dish
 
-# Функціональний view для всіх страв
+# Функціональний view для всіх страв з пошуком
 def dish_list(request):
+    query = request.GET.get("q")
+    dishes = Dish.objects.all()
+    if query:
+        dishes = dishes.filter(name__icontains=query)
+
     categories = Category.objects.prefetch_related("dishes").all()
-    return render(request, "kitchen/dish_list.html", {"categories": categories})
+    return render(
+        request,
+        "kitchen/dish_list.html",
+        {"categories": categories, "dishes": dishes, "query": query},
+    )
 
 # Функціональний view для страв конкретної категорії
 def category_dishes(request, pk):
     category = get_object_or_404(Category.objects.prefetch_related("dishes"), pk=pk)
-    return render(request, "kitchen/dish_list.html", {"categories": [category]})
+    query = request.GET.get("q")
+    dishes = category.dishes.all()
+    if query:
+        dishes = dishes.filter(name__icontains=query)
+
+    return render(
+        request,
+        "kitchen/dish_list.html",
+        {"categories": [category], "dishes": dishes, "query": query},
+    )
 
 # Клас‑based view для категорій
 class CategoryListView(ListView):
