4be06dbed7c65b0763303cd5af09f81376f1fe88
Ihor Zasimenko <boxletter2011@gmail.com>
2026-02-19 18:29:25 +0200

    Add pagination for dishes list

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from django.views.generic import ListView
 from .models import Category, Dish
 
-# Функціональний view для всіх страв з пошуком
+# Функціональний view для всіх страв з пошуком і пагінацією
 def dish_list(request):
     query = request.GET.get("q")
     dishes = Dish.objects.all()
     if query:
         dishes = dishes.filter(name__icontains=query)
 
+    paginator = Paginator(dishes, 6)  # 6 страв на сторінку
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     categories = Category.objects.prefetch_related("dishes").all()
     return render(
         request,
         "kitchen/dish_list.html",
-        {"categories": categories, "dishes": dishes, "query": query},
+        {"categories": categories, "page_obj": page_obj, "query": query},
     )
 
-# Функціональний view для страв конкретної категорії
+# Функціональний view для страв конкретної категорії з пошуком і пагінацією
 def category_dishes(request, pk):
     category = get_object_or_404(Category.objects.prefetch_related("dishes"), pk=pk)
     query = request.GET.get("q")
@@ -24,10 +29,14 @@ def category_dishes(request, pk):
     if query:
         dishes = dishes.filter(name__icontains=query)
 
+    paginator = Paginator(dishes, 6)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     return render(
         request,
         "kitchen/dish_list.html",
-        {"categories": [category], "dishes": dishes, "query": query},
+        {"categories": [category], "page_obj": page_obj, "query": query},
     )
 
 # Клас‑based view для категорій
