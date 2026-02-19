09c2f0f91af8f42b46d1f1db8945fc79528b5334
Ihor Zasimenko <boxletter2011@gmail.com>
2026-02-19 19:08:01 +0200

    Enhance admin customization: add filters, ordering, and extended search

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -5,10 +5,13 @@ from .models import Dish, Category
 @admin.register(Dish)
 class DishAdmin(admin.ModelAdmin):
     list_display = ("name", "price", "category")
-    search_fields = ("name",)
+    search_fields = ("name", "category__name")
+    list_filter = ("category",)
+    ordering = ("name",)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("name",)
     search_fields = ("name",)
+    ordering = ("name",)
