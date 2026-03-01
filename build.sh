e35cae0edacacbc24b42737ad707b061355d267e
Ihor Zasimenko <boxletter2011@gmail.com>
2026-03-01 17:59:22 +0200

    Update build.sh to copy media files for Render

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Встановлення залежностей
 pip install -r requirements.txt
+
+# Збір статичних файлів
 python manage.py collectstatic --no-input
+
+# Міграції
 python manage.py migrate
+
+# Копіювання медіа у контейнер (щоб Render бачив фото)
+mkdir -p $PWD/media
+cp -r media/* $PWD/media/
